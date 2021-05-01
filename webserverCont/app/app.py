2a3ae08519ddb5ca647bfc03f248565f740610f6
root <root@ip-172-31-64-8.ec2.internal>
2021-05-01 06:49:11 +0000

    errors

--- webserverCont/app/app.py
+++ webserverCont/app/app.py
@@ -12,18 +12,18 @@ def is_compressed(filename):
 
 def compress(filename,flag):
     request = ""
-    if(flag==0): request = "zip:81/gzip?filename="+filename
-    else: request = "zip:81/gunzip?filename="+filename
+    if(flag==0): request = "zipcont:81/gzip?filename="+filename
+    else: request = "zipcont:81/gunzip?filename="+filename
     output = subprocess.Popen(["curl" ,request],stdout=subprocess.PIPE).communicate()[0]
     return output
 
 def uploadfile(filepath):
-    request = "storage:80/upload?filepath="+filepath
+    request = "storagecont:80/upload?filepath="+filepath
     uopt = subprocess.Popen(["curl",request],stdout=subprocess.PIPE).communicate()[0]
     return uopt
 
 def downloadfile(filename):
-    request = "storage:80/getfile?filename="+filename
+    request = "storagecont:80/getfile?filename="+filename
     dopt = subprocess.Popen(["curl",request],stdout=subprocess.PIPE).communicate()[0]
     return dopt
 
